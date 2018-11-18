b6fb5049f579736e8d387889929cf0ca69b3b9f8
strahinja <keseljs@gmail.com>
2018-11-18 20:56:06 +0100

    add flask app

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/')
+def hello_world():
+    return 'Hello, Stra'
+
+
+@app.route('/test/<foo>')
+def new_name(foo):
+    answer = "Hello " + foo
+    return answer
